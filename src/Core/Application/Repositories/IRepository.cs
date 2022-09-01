70808237a2f5abf6b8af7b7a9cb52256b502bea6
Ömer Furkan DOĞRUYOL <dogruyolomerfurkan@gmail.com>
2022-09-01 19:22:01 +0300

    read and write repository interfaces completed

---
+++ src/Core/Application/Repositories/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories;
+
+public interface IRepository<T> where T : BaseEntity, new()
+{
+    DbSet<T> Entity { get; }
+}
---
+++ src/Core/Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.Common;
+
+namespace Application.Repositories;
+
+public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity, new()
+{
+    Task<bool> AddAsync(T entity);
+    Task AddRangeAsync(IEnumerable<T> entities);
+    bool Update(T entity);
+    void UpdateRange(IEnumerable<T> entities);
+    bool Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
+    Task<int> SaveAsync();
+}
